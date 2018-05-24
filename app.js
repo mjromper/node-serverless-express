74a6a5e666ac8e3761c94bc90c3b3ce173743017
Manuel Romero <manuel.romero@qlik.com>
2018-05-24 17:59:47 +0200

    a ver que pasa

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 var express = require('express');
 var path = require("path");
+var bodyParser = require('body-parser');
 var app = express();
 
 const root = './client';
+
+app.use(bodyParser.json());
 
 app.use(express.static( path.resolve(__dirname, root)));
 app.use("/bower_components", express.static(path.resolve(__dirname, './wwwroot/npm'), { maxAge: 86400000*7 }));
 
 app.get('/api/hello', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
+  res.json({
+    "method": 'get /api/hello',
+    "query": req.query,
+    "body": req.body
   });
 });
 
 app.post('/api/hello', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
+  res.json({
+    "method": 'post /api/hello',
+    "query": req.query,
+    "body": req.body
   });
 });
 
-app.get('/ping', function(req, res) {
+app.get('/api/ping', function(req, res) {
+  res.json({
+    "method": 'get /api/ping',
+    "query": req.query,
+    "body": req.body
+  });
+});
+
+app.get('/api/pong/get', function(req, res) {
   res.send({
-    "Output": "pong!"
+    "method": 'get /api/pong/get',
+    "query": req.query,
+    "body": req.body
   });
 });
 
